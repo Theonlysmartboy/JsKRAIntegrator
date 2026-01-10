cdbf2809256e0b8a576b62c4ce355b9956a1f356
Theonlysmartboy <o2jose43@gmail.com>
2026-01-10 12:10:35 +0300

    Completed the Main Module

---
+++ JsKRAIntegrator/Main/ApiEndpoints.vb
@@ -0,0 +1,18 @@
+﻿Namespace Main
+    Public Class ApiEndpoints
+        Public Const SELECT_INIT As String = "/initializer/selectInitInfo"
+        Public Const CODE_DATA As String = "/code/selectCodes"
+        Public Const ITEM_CLASSIFICATION_SELECTOR As String = "/itemClass/selectItemsClass"
+        Public Const CUSTOMER_SELECTOR As String = "/customers/selectCustomer"
+        Public Const BRANCH_SELECTOR As String = "/branches/selectBranches"
+        Public Const BRANCH_INFO As String = "/branch/info"
+        Public Const ITEM_SAVE As String = "/items/saveItems"
+        Public Const ITEM_SELECT As String = "/items/selectItems"
+        Public Const IMPORTED_ITEM_SELECT As String = "/imports/selectImportItems"
+        Public Const IMPORTED_ITEM_UPDATE As String = "/imports/updateImportItems"
+        Public Const SALES As String = "/trnsSales/saveSales"
+        Public Const PURCHASE As String = "/trnsPurchase/selectTrnsPurchaseSales"
+        Public Const STOCK_SAVE As String = "/stockMaster/saveStockMaster"
+        Public Const STOCK_SELECT As String = "/stockMaster/selectStockMaster"
+    End Class
+End Namespace
