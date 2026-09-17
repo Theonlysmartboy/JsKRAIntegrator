7dae0bfc201f76b73cc4139e96fa90973e4e8fe5
Theonlysmartboy <o2jose43@gmail.com>
2026-09-17 17:30:12 +0300

    Minor update

--- Ui/Sales.vb
+++ Ui/Sales.vb
@@ -430,7 +430,7 @@ Public Class Sales
         ' If you add a real print button in the designer, hook it to this handler.
         If fullReceiptBitmap Is Nothing Then
             CustomAlert.ShowAlert(Me, "No receipt to print. Send invoice first.", "Print",
-                                  CustomAlert.AlertType.Warning, CustomAlert.ButtonType.OK)
+                                CustomAlert.AlertType.Warning, CustomAlert.ButtonType.OK)
             Return
         End If
         Dim dlg As New PrintDialog()
@@ -460,8 +460,7 @@ Public Class Sales
         Using sliceBmp As New Bitmap(fullReceiptBitmap.Width, sliceHeightSource)
             Using gSlice As Graphics = Graphics.FromImage(sliceBmp)
                 gSlice.DrawImage(fullReceiptBitmap, New Rectangle(0, 0, sliceBmp.Width, sliceBmp.Height),
-                                 New Rectangle(0, currentPrintY, sliceBmp.Width, sliceHeightSource),
-                                 GraphicsUnit.Pixel)
+                        New Rectangle(0, currentPrintY, sliceBmp.Width, sliceHeightSource), GraphicsUnit.Pixel)
             End Using
             ' Draw scaled slice onto page
             e.Graphics.DrawImage(sliceBmp, e.MarginBounds.Left, e.MarginBounds.Top, pageWidth, CInt(sliceHeightSource * scale))
@@ -499,8 +498,9 @@ Public Class Sales
             Return Nothing
         End If
         Dim result As DateTime
-        If DateTime.TryParseExact(value, "yyyyMMddHHmmss", Globalization.CultureInfo.InvariantCulture, Globalization.DateTimeStyles.None,
-                              result) Then
+        If DateTime.TryParseExact(value, "yyyyMMddHHmmss",
+                Globalization.CultureInfo.InvariantCulture,
+                Globalization.DateTimeStyles.None, result) Then
             Return result
         End If
         Return Nothing
